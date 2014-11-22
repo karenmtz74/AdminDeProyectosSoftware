ea59b1e72b0252945c7aabc676e421d5b4b7d81c
karenmtz74 <ana_sm74@hotmail.com>
2014-11-22 11:33:22 -0600

    Update Conexion.java

--- src/bpp/Conexion.java
+++ src/bpp/Conexion.java
@@ -1,18 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package bpp;
+
+/**
+ *
+ * @author ana_sm74
+ */
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author Loreto
- */
+
 public class Conexion {
      public Connection getConexion() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
         Connection conexion = null;
         Class.forName("com.mysql.jdbc.Driver").newInstance();
-        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=convertToNull&user=root&password=root");
+        
+        //si alguien tiene contraseña en su mysql a lado del igual de password se le agrega
+        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=convertToNull&user=root&password=");
         return conexion;
     }   
 }
