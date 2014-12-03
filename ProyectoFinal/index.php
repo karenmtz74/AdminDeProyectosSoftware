7b6521a149c4eb5bc7510848751631e078954f6e
karen <ana_sm74@hotmail.com>
2014-12-03 14:22:33 -0600

    cambios
    cambios

---
+++ ProyectoFinal/index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Bottle Pet Print</title>
+<link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />
+<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		
+		<meta name="description" content="Bottle Pet Print" />
+		<meta name="keywords" content="bottle, pet, print" />
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="css/content.css" />
+		<script src="js/modernizr.custom.js"></script>
+</head>
+
+<body>
+    <div class="content">
+    	<p align="center"><img src="img/10834064_10152353163662563_1494124012_n.jpg" /></p>
+			<section>
+				
+				<div class="mockup-content">
+					<div class="morph-button morph-button-modal morph-button-modal-2 morph-button-fixed">
+						<button type="button">Login</button>
+						<div class="morph-content">
+							<div>
+								<div class="content-style-form content-style-form-1">
+									<span class="icon icon-close">Close the dialog</span>
+									<h2>Login</h2>
+									<form id="login" name="login" method="post" action="login.php" onSubmit="return validar();" >
+										<p><label>User</label><input type="text" id="idEstudiantes" name="idEstudiantes"/></p>
+										
+									</form>
+								</div>
+							</div>
+						</div>
+					</div><!-- morph-button -->
+				</div><!-- /form-mockup -->
+			</section>
+		<script src="js/classie.js"></script>
+        <script languaje="javascript">
+		
+function validar(){
+	if(document.login.idUsuarios.value==''){
+		alert('Faltan campos que llenar');
+		document.login.idUsuarios.focus();
+		return false;
+	}
+	if(document.login.contrasena.value==''){
+		alert('Faltan campos que llenar');
+		document.login.contrasena.focus();
+		return false;
+	}
+}
+
+</script>
+		<script src="js/uiMorphingButton_fixed.js"></script>
+		<script>
+			(function() {
+				var docElem = window.document.documentElement, didScroll, scrollPosition;
+
+				// trick to prevent scrolling when opening/closing button
+				function noScrollFn() {
+					window.scrollTo( scrollPosition ? scrollPosition.x : 0, scrollPosition ? scrollPosition.y : 0 );
+				}
+
+				function noScroll() {
+					window.removeEventListener( 'scroll', scrollHandler );
+					window.addEventListener( 'scroll', noScrollFn );
+				}
+
+				function scrollFn() {
+					window.addEventListener( 'scroll', scrollHandler );
+				}
+
+				function canScroll() {
+					window.removeEventListener( 'scroll', noScrollFn );
+					scrollFn();
+				}
+
+				function scrollHandler() {
+					if( !didScroll ) {
+						didScroll = true;
+						setTimeout( function() { scrollPage(); }, 60 );
+					}
+				};
+
+				function scrollPage() {
+					scrollPosition = { x : window.pageXOffset || docElem.scrollLeft, y : window.pageYOffset || docElem.scrollTop };
+					didScroll = false;
+				};
+
+				scrollFn();
+
+				[].slice.call( document.querySelectorAll( '.morph-button' ) ).forEach( function( bttn ) {
+					new UIMorphingButton( bttn, {
+						closeEl : '.icon-close',
+						onBeforeOpen : function() {
+							// don't allow to scroll
+							noScroll();
+						},
+						onAfterOpen : function() {
+							// can scroll again
+							canScroll();
+						},
+						onBeforeClose : function() {
+							// don't allow to scroll
+							noScroll();
+						},
+						onAfterClose : function() {
+							// can scroll again
+							canScroll();
+						}
+					} );
+				} );
+
+				// for demo purposes only
+				[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
+					bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
+				} );
+			})();
+		</script>
+    </div>
+</body>
+</html>
---
+++ ProyectoFinal/usuario.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Bottle Pet Print</title>
+<link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />
+</head>
+
+<body>
+<div class="content">
+<table>
+<tr><td>
+<img src="img/10834064_10152353163662563_1494124012_n.jpg" height="150"/> <br><br><br><br><br><br><br><br><br><br><br><br><br><br></td><td width="15%"></td><td><br><br><br><br><br>
+<?php $idEstudiante=$_GET['idEstudiante'];
+		 ?>
+        
+<?php $enlace =  mysql_connect('localhost', 'root', 'root');
+if (!$enlace) {
+    die('No pudo conectarse: ' . mysql_error());
+}
+
+mysql_select_db('bpp') or die('No se pudo seleccionar la base de datos');
+
+
+
+$query = 'SELECT * FROM estudiante where idEstudiante='.$idEstudiante.'';
+$result = mysql_query($query) ;
+if (mysql_num_rows($result) == 0) {
+    echo "No se han encontrado filas, nada a imprimir, asi que voy a detenerme.";
+    exit;
+}
+
+while ($fila = mysql_fetch_assoc($result)) {
+	$est=$fila["idEstudiante"];
+	echo '<p align="center">Mi Perfil </p>';
+	
+	echo '<p align="center">'.$fila['nombre'].' </p>';
+	
+	echo '<p align="center">'.$fila['idEstudiante'].' </p>';
+	
+	echo '<p align="center">'.$fila['carrera'].' </p>';
+	
+	echo '<p align="center">Saldo: $'.$fila['saldo'].' </p>';
+}
+
+mysql_free_result($resultado);
+
+mysql_close($enlace);?>
+</td><td></td><td width="20%"></td></tr>
+<tr>
+<td></td>
+<td><button class="btn" onClick="location.href='index.php'">Cerrar Sesión</button></td>
+<td></td>
+<td><button class="btn">Insertar Botellas</button></td>
+</tr></table>
+
+</div>
+</body>
+</html>
